1fe9c26ce3a7d2465a78367fa9b8e8a1d870b79b
natvvgls <natalia.d.m975@gmail.com>
2022-08-23 13:20:48 -0500

    Imágenes y secciones añadidas

--- src/pages/Sala2.jsx
+++ src/pages/Sala2.jsx
@@ -6,20 +6,150 @@ const Sala2 = () => {
     <>
       <section
         id='section1'>
-        {/*typo A*/}
+        {/*type X*/}
       </section>
       <section
         id='section2'>
-        {/*typo A*/}
+        {/*type X*/}
       </section>
       <section
         id='section3'>
+        {/*type X*/}
+      </section>
 
+      <section
+        id='section4'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section5'>
+        {/*type C*/}
       </section>
 
       <section
         id='section6'>
+        {/*type B*/}
+      </section>
 
+      <section
+        id='section7'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section8'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section9'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section10'>
+        {/*type C*/}
+      </section>
+
+      <section
+        id='section11'>
+        {/*type B*/}
+      </section>
+
+      <section
+        id='section12'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section13'>
+        {/*type B*/}
+      </section>
+
+      <section
+        id='section14'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section15'>
+        {/*type C*/}
+      </section>
+
+      <section
+        id='section16'>
+        {/*type B*/}
+      </section>
+
+      <section
+        id='section17'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section18'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section19'>
+        {/*type B*/}
+      </section>
+
+      <section
+        id='section20'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section21'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section22'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section23'>
+        {/*type C*/}
+      </section>
+
+      <section
+        id='section24'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section25'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section26'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section27'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section28'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section29'>
+        {/*type X*/}
+      </section>
+
+      <section
+        id='section30'>
+        {/*type X*/}
       </section>
     </>
 
