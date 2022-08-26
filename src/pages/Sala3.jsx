7c10a1d32d54a3d5746bd16a1e5169fc7ab903de
Robert <robertquillaslopez@gmail.com>
2022-08-26 10:31:57 -0500

    sala3 preparado

--- src/pages/Sala3.jsx
+++ src/pages/Sala3.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Sala3 = () => {
-  const dataSala2 = require('../bd/Sala2.json')
+  const dataSala3 = require('../bd/Sala3.json')
+  const istablet = useMediaQuery({query: '(min-width: 1280px)'})
+
   return (
-    <div>
-      HOLA: SALA3
+    <div
+      className='pt-[85px] overflow-x-hidden'>
+      <section
+        id='section1'
+        className='imgBack bg-slate-500 relative 
+        xl:bg-[url("/public/imagenes/sala2/hero-sala2-min.jpg")] '>
+        {
+          !istablet &&
+            <img 
+              src='./imagenes/sala2/hero-sala2-min.jpg' 
+              alt=''
+              className=' z-0'
+            />
+        }
+        <div
+          className='contSalas flex top-[-10%] md:top-0  absolute xl:top-0 xl:relative'>
+          <h1 
+            className='text-left w-auto mt-[3rem] text-[20px] ml-[17%] 
+            sm:w-[100px] md:text-[32px] md:w-[150px]
+            xl:ml-0 xl:text-[56px] xl:w-auto'>
+            SALA 3
+            <span className='lineAux'/>
+          </h1>
+        </div>
+      </section>
     </div>
   )
 }
