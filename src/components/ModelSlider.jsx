ba747fc053f1a0ce83222d62df6dacc3daf2d317
Robert <robertquillaslopez@gmail.com>
2022-11-11 16:53:21 -0500

    lista

--- src/components/ModelSlider.jsx
+++ src/components/ModelSlider.jsx
@@ -9,52 +9,66 @@ import Slider from './Slider'
 const ModelSlider = (props) => {
     const numParrafos = props.contenido.length
 
-    function OptionSlider(){
-        if(props.type === 'Recorted'){
-            return <SliderRecorted state={props.state} {...props.imagenes}/>
-        }else if (props.type === 'Recorted2'){
-            return <SliderRecorted2 state={props.state} {...props.imagenes}/>
-        }else if (props.type === 'Recorted3'){
-            return <SliderRecorted3 state={props.state} {...props.imagenes}/>
-        }else if (props.type === 'Recorted4'){
-            return <SliderRecorted4 state={props.state} {...props.imagenes}/>
-        }else if (props.type === 'Recorted5'){
-            return <SliderRecorted5 state={props.state} {...props.imagenes}/>
-        }else{
-            return <Slider state={props.state} {...props.imagenes}/>
+    function OptionSlider() {
+        if (props.type === 'Recorted') {
+            return <SliderRecorted state={props.state} {...props.imagenes} />
+        } else if (props.type === 'Recorted2') {
+            return <SliderRecorted2 state={props.state} {...props.imagenes} />
+        } else if (props.type === 'Recorted3') {
+            return <SliderRecorted3 state={props.state} {...props.imagenes} />
+        } else if (props.type === 'Recorted4') {
+            return <SliderRecorted4 state={props.state} {...props.imagenes} />
+        } else if (props.type === 'Recorted5') {
+            return <SliderRecorted5 state={props.state} {...props.imagenes} />
+        } else {
+            return <Slider state={props.state} {...props.imagenes} />
         }
     }
 
-  return (
-    <div className='contSalas relative '>
-        { props.indicador &&
-            <ul 
-               className='pl-[4.5%] pb-[18px] sm:pb-[24px] lg:pb-[40px] sm:pl-[2.5%]' 
-               style={{listStyleType: "disc"}}>
-               <li 
-                  style={{color: props.colorIndicador}}
-                  className='font-[600] font-Public leading-[16.24px] text-[14px]
+    return (
+        <div className='contSalas relative '>
+            {props.indicador &&
+                <ul
+                    className='pl-[4.5%] pb-[18px] sm:pb-[24px] lg:pb-[40px] sm:pl-[2.5%]'
+                    style={{ listStyleType: "disc" }}>
+                    <li
+                        style={{ color: props.colorIndicador }}
+                        className='font-[600] font-Public leading-[16.24px] text-[14px]
                   sm:leading-[31.68px] sm:text-[20px] lg:text-[24px]'>
-                  {props.indicador}
-               </li>
-            </ul>
-        }
-        <h2 className='uppercase'>{props.title}</h2>
-        <p className='mtPharagraphs'>
-            {props.contenido.map( 
-                (parrafo,index) => {
-                    if(index === numParrafos-1){
-                        return <span key={props.title+index}>{parrafo}</span>
-                    }else{
-                        return <span key={props.title+index}>{parrafo}<br/><br/></span>
+                        {props.indicador}
+                    </li>
+                </ul>
+            }
+            <h2 className='uppercase'>{props.title}</h2>
+            <p className='mtPharagraphs'>
+                {props.contenido.map(
+                    (parrafo, index) => {
+                        if (index === numParrafos - 1) {
+                            return <span key={props.title + index}>{parrafo}</span>
+                        } else {
+                            return <span key={props.title + index}>{parrafo}<br /><br /></span>
+                        }
                     }
-                }
-            )}
-        </p>
-        
-        {OptionSlider()}
-    </div>
-  )
+                )}
+            </p>
+            {
+                props.listaStatus &&
+                <ul className='mtPharagraphs'>
+                    {props.lista.map(
+                        (parrafo, index) => {
+                            if (index === numParrafos - 1) {
+                                return <p key={props.title + index}>{parrafo}</p>
+                            } else {
+                                return <p key={props.title + index}>{parrafo}<br /><br /></p>
+                            }
+                        }
+                    )}
+                </ul>
+            }
+
+            {OptionSlider()}
+        </div>
+    )
 }
 
 export default ModelSlider
